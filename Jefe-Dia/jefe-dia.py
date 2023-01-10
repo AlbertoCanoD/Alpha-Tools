edc3cd3e004a29516090ef6b41c9ab8932722924
Alberto Cano <canoalboroteo@gmail.com>
2023-01-10 18:48:53 +0100

    añadida excepción en la entrada de números y si existen menos responsables que días

--- Jefe-Dia/jefe-dia.py
+++ Jefe-Dia/jefe-dia.py
@@ -4,17 +4,27 @@ import random
 responsables = ["Alberto", "Dani.R", "Dani.J", "Mari", "Mario", "Maria.D", "Alex.H",
                 "Fernando", "Ema", "Clara", "Natalia", "Almudena", "Espe", "Raquel", "Selene", "Sonia"]
 
-# Introduccion de días de campamento
-dias = input("Número de Días del campamento: ")
+# Introduccion de días de campamento, sólo se pueden introducir números
+while True:
+    try:
+        dias = int(input("Número de Días del campamento: "))
+        break
+    except ValueError:
+        print("¡¡Se debe introducir un número!!")
 
-# Se hace una lista con responsables sin repeticion
-eleccion = random.sample(responsables, int(dias))
+# Si se puede emparejar cada respon a un día
+if len(responsables) > dias:
 
-# Sin repeticion de responsables
-for i in range(int(dias)):
-    print(f"Dia {i + 1}, Jefe de Día: " + eleccion[i])
+    # Se hace una lista con responsables sin repeticion
+    eleccion_sin_repeticion = random.sample(responsables, dias)
 
-# Con repeticion de responsables
-'''for i in range(int(dias)):
-    print(f"Dia {i + 1}, Jefe de Día: " + random.choice(responsables))
-'''
+    # Se itera para cada día
+    for i in range(int(dias)):
+        # Sin repeticion de responsables
+        print(f"Dia {i + 1}, Jefe de Día: " + eleccion_sin_repeticion[i])
+
+# Si hay más días que responsables
+else:
+    for i in range(int(dias)):
+        # Con repeticion de responsables
+        print(f"Dia {i + 1}, Jefe de Día: " + random.choice(responsables))
